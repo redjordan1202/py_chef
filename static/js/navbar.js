08fdda44735921d1060ce82ed27a7d0a6bed5117
Jordan <redjordan123@gmail.com>
2023-09-01 19:09:06 -0700

    Created navbar.js
    Creating main JS script to handle dropdown navbar controls.

---
+++ static/js/navbar.js
@@ -0,0 +1,19 @@
+function expand_navbar() {
+    dropdown = document.getElementById("navbar-dropdown");
+    console.log("clicked")
+    if (dropdown.classList.contains("hidden")) {
+        dropdown.classList.remove("hidden");
+        dropdown.style.display = "flex";
+    } else {
+        dropdown.classList.add("hidden");
+        dropdown.style.display = '';
+    }
+}
+
+function set_events() {
+    console.log("setting events")
+    navbar_expand = document.getElementsByClassName("navbar-expand");
+    for (let x of navbar_expand) {
+        x.addEventListener("click", expand_navbar);
+    }
+}
